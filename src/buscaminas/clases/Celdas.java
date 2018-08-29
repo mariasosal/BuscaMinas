465786543c180c17fbcb0f5297319cf53fd583f3
LENOVO <LENOVO@192.168.1.13>
2018-08-28 23:24:08 -0500

    :((

---
+++ src/buscaminas/clases/Celdas.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package buscaminas.clases;
+ 
+/**
+ *
+ * @author LENOVO
+ */
+public class Celdas {
+    private int x;
+    private int y;
+    private int tipocelda;
+
+    public Celdas(int x, int y, int tipocelda) {
+        this.x = x;
+        this.y = y;
+        this.tipocelda = tipocelda;
+    }
+    
+    
+}
